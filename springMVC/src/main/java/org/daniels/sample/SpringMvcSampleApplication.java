687a6f9d079cf588569271e62fa1484991c9960f
Daniels <daniel.sadowski75@gmail.com>
2017-04-04 23:48:03 +0200

    changed package name

--- springMVC/src/main/java/org/daniels/sample/SpringMvcSampleApplication.java
+++ springMVC/src/main/java/org/daniels/sample/SpringMvcSampleApplication.java
@@ -1,10 +1,11 @@
-package org.daniel.sample;
+package org.daniels.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMvcSampleApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcSampleApplication.class, args);
--- springMVC/src/test/java/org/daniels/sample/SpringMvcSampleApplicationTests.java
+++ springMVC/src/test/java/org/daniels/sample/SpringMvcSampleApplicationTests.java
@@ -1,4 +1,4 @@
-package org.daniel.sample;
+package org.daniels.sample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
