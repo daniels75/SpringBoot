7caf3bfddc1e66a50370b431017b730046ece4a8
Daniels <daniel.sadowski75@gmail.com>
2017-04-15 14:59:56 +0200

    Added Materlize css

--- springMVC/src/main/java/org/daniels/sample/controller/TwitterController.java
+++ springMVC/src/main/java/org/daniels/sample/controller/TwitterController.java
@@ -2,11 +2,16 @@ package org.daniels.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Created by daniels on 04.04.2017.
@@ -20,15 +25,17 @@ public class TwitterController {
     @RequestMapping("/")
     public String hello(Model model, @RequestParam(value = "search", defaultValue = "twitter default") String search) {
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
 
-        model.addAttribute("message", text);
+        List<Tweet> tweets = searchResults.getTweets();
+
+        model.addAttribute("tweets", tweets);
+        model.addAttribute("search", search);
         return "resultPage";
     }
 
     @RequestMapping("/info")
     public String info(Model model, @RequestParam(value = "name", defaultValue = "word")  String userName) {
-        model.addAttribute("message", "Witaj " + userName +  "!");
-        return "resultPage";
+        model.addAttribute("message", "Hello " + userName +  "!");
+        return "info";
     }
 }
