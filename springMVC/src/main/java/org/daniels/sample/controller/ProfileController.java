a6f75288c75000abb40e2f56e3e8fc790247326f
Daniels <daniel.sadowski75@gmail.com>
2017-04-30 01:32:48 +0200

    Step 3.3 - added a list to the form in the profile page

--- springMVC/src/main/java/org/daniels/sample/controller/ProfileController.java
+++ springMVC/src/main/java/org/daniels/sample/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -17,13 +18,12 @@ import java.util.Locale;
 @Controller
 public class ProfileController {
 
-
     @RequestMapping("/profile")
     public String displayProfile(ProfileForm profileForm) {
         return "profile/profilePage";
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @RequestMapping(value = "/profile", params = {"save"}, method = RequestMethod.POST)
     public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "profile/profilePage";
@@ -35,5 +35,18 @@ public class ProfileController {
     @ModelAttribute("dateFormat")
     public String localFormat(Locale locale) {
         return USLocalDateFormatter.getPattern(locale);
+    }
+
+    @RequestMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", params={"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest httpServletRequest) {
+        Integer rowId = Integer.valueOf(httpServletRequest.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
     }
 }
